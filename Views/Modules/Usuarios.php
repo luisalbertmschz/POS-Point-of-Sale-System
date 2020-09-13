10887ceb6c1c22ee4078cf4e811ec51187eeba3d
luisalbertmschz <cerebrito07@hotmail.com>
2020-09-13 04:10:01 -0400

    [🚣] Actualización | mejoras a Módulo de usuarios, Dynamic-responsive users table view

--- Views/Modules/Usuarios.php
+++ Views/Modules/Usuarios.php
@@ -27,25 +27,130 @@
 
         <div class="box-body">
 
+          <table class="table table-bordered table-condensed table-hover dt-responsive tablaUsuarios" width="100%">
 
-
-          <table class="table table-bordered table-striped">
             <thead>
+
+              <tr>
+                <th>#</th>
+                <th>Usuario</th>
+                <th>Nombre</th>
+                <th>Foto</th>
+                <th>Rol</th>
+                <th>Estatus</th>
+                <th>Último login</th>
+                <th>Acciones avanzadas</th>
+              </tr>
 
             </thead>
 
-            <tr>
-              <th>#</th>
-              <th>Usuario</th>
-              <th>Nombre</th>
-              <th>Foto</th>
-              <th>Rol</th>
-              <th>Estatus</th>
-              <th>Último login</th>
-              <th>Action</th>
-            </tr>
-
             <tbody>
+              <tr>
+                <td>1</td>
+                <td>WKNDAKNG01</td>
+                <td>T'Challa</td>
+                <td><img src="Views/img/Usuarios/Default/WKND.jpg" class="user-image img-thumbnail" width="60x"></td>
+                <td>Admin</td>
+                <td><button class="btn btn-success btn-xs">Wakanda Forever</button></td>
+                <td>2020-08-29 00:00:00</td>
+                <td>
+                  <div class="btn-group">
+                    <button class="btn btn-warning"><i class="fas fa-user-edit"></i></button>
+                    <button class="btn btn-danger"><i class="fas fa-user-times"></i></button>
+
+
+                  </div>
+
+
+                </td>
+
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>WKNDAKNG01</td>
+                <td>T'Challa</td>
+                <td><img src="Views/img/Usuarios/Default/WKND.jpg" class="user-image img-thumbnail" width="60x"></td>
+                <td>Ventas</td>
+                <td><button class="btn btn-danger btn-xs">Wakanda Forever</button></td>
+                <td>2020-08-29 00:00:00</td>
+                <td>
+                  <div class="btn-group">
+                    <button class="btn btn-warning"><i class="fas fa-user-edit"></i></button>
+                    <button class="btn btn-danger"><i class="fas fa-user-times"></i></button>
+
+
+                  </div>
+
+
+                </td>
+
+              </tr>
+
+              <tr>
+                <td>1</td>
+                <td>WKNDAKNG01</td>
+                <td>T'Challa</td>
+                <td><img src="Views/img/Usuarios/Default/WKND.jpg" class="user-image img-thumbnail" width="60x"></td>
+                <td>Admin</td>
+                <td><button class="btn btn-success btn-xs">Wakanda Forever</button></td>
+                <td>2020-08-29 00:00:00</td>
+                <td>
+                  <div class="btn-group">
+                    <button class="btn btn-warning"><i class="fas fa-user-edit"></i></button>
+                    <button class="btn btn-danger"><i class="fas fa-user-times"></i></button>
+
+
+                  </div>
+
+
+                </td>
+
+              </tr>
+
+              <tr>
+                <td>1</td>
+                <td>WKNDAKNG01</td>
+                <td>T'Challa</td>
+                <td><img src="Views/img/Usuarios/Default/WKND.jpg" class="user-image img-thumbnail" width="60x"></td>
+                <td>Admin</td>
+                <td><button class="btn btn-success btn-xs">Wakanda Forever</button></td>
+                <td>2020-08-29 00:00:00</td>
+                <td>
+                  <div class="btn-group">
+                    <button class="btn btn-warning"><i class="fas fa-user-edit"></i></button>
+                    <button class="btn btn-danger"><i class="fas fa-user-times"></i></button>
+
+
+                  </div>
+
+
+                </td>
+
+              </tr>
+
+
+              <tr>
+                <td>1</td>
+                <td>WKNDAKNG01</td>
+                <td>T'Challa</td>
+                <td><img src="Views/img/Usuarios/Default/WKND.jpg" class="user-image img-thumbnail" width="60x"></td>
+                <td>Admin</td>
+                <td><button class="btn btn-warning btn-xs">Wakanda Forever</button></td>
+                <td>2020-08-29 00:00:00</td>
+                <td>
+                  <div class="btn-group">
+                    <button class="btn btn-warning"><i class="fas fa-user-edit"></i></button>
+                    <button class="btn btn-danger"><i class="fas fa-user-times"></i></button>
+
+
+                  </div>
+
+
+                </td>
+
+              </tr>
+
+
               <tr>
                 <td>1</td>
                 <td>WKNDAKNG01</td>
@@ -94,9 +199,6 @@
 //-----------------------------------------------//
  -->
 
-  <!-- Trigger the modal with a button -->
-  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
-
   <!-- Modal -->
 
   <div id="modalAñadirUsuario" class="modal fade" role="dialog">
@@ -108,134 +210,134 @@
 
         <form role="form" method="pos" enctype="multipart/form-data">
 
-              <!-- 
+          <!-- 
             //-------------------------------------------------//
             //       HHEADER VENTANA MODAL                    //
             //-----------------------------------------------//
             -->
 
-              <div class="modal-header" style=" background-color:#00a65a; color:white; font-weight: bold; ">
+          <div class="modal-header" style=" background-color:#00a65a; color:white; font-weight: bold; ">
 
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
 
-                <h4 class="modal-title"><i class="fas fa-users"></i> Formulario de nuevo usuario </h4>
+            <h4 class="modal-title"><i class="fas fa-users"></i> Formulario de nuevo usuario </h4>
 
-              </div>
+          </div>
 
 
-              <!-- 
+          <!-- 
             //-------------------------------------------------//
             //       BODY VENTANA MODAL                    //
             //-----------------------------------------------//
             -->
 
-              <div class="modal-body">
+          <div class="modal-body">
 
-                <div class="box-body">
+            <div class="box-body">
 
-                  <!-- SECCION NOMBRE COMPLETO DEL USUARIO -->
+              <!-- SECCION NOMBRE COMPLETO DEL USUARIO -->
 
-                  <div class="form-group">
+              <div class="form-group">
 
-                    <div class="input-group">
+                <div class="input-group">
 
-                      <span class="input-group-addon"> <i class="fas fa-user-alt"></i></span>
+                  <span class="input-group-addon"> <i class="fas fa-user-alt"></i></span>
 
-                      <input type="text" class="form-control input-lg" name="RegitroNombre" placeholder="Ingresar nombre" required>
+                  <input type="text" class="form-control input-lg" name="RegitroNombre" placeholder="Ingresar nombre" required>
 
-
-                    </div>
-
-                  </div>
-
-
-                  <!-- SECCION NOMBRE DEL USUARIO -->
-
-                  <div class="form-group">
-
-                    <div class="input-group">
-
-                      <span class="input-group-addon"> <i class="fas fa-user-tag"></i></span>
-
-                      <input type="text" class="form-control input-lg" name="RegitroUsuario" placeholder="Ingresar usuario" required>
-
-                    </div>
-
-                  </div>
-
-
-                  <!-- SECCION CONTRASEÑA DEL USUARIO -->
-
-                  <div class="form-group">
-
-                    <div class="input-group">
-
-                      <span class="input-group-addon"> <i class="fas fa-key"></i></span>
-
-                      <input type="text" class="form-control input-lg" name="RegitroPassword" placeholder="Ingresar la contraseña" required>
-
-                    </div>
-
-                  </div>
-
-
-
-                  <!-- SECCION PERFIL DEL USUARIO -->
-
-                  <div class="form-group">
-
-                    <div class="input-group">
-
-                      <span class="input-group-addon"><i class="fas fa-user-shield"></i></span>
-
-                      <select class="form-control input-lg" name="RegistroPerfil">
-
-                        <option value="">Seleccionar el rol</option>
-
-                        <option value="Administrador">Administrador</option>
-
-                        <option value="Inventario">Inventario</option>
-
-                        <option value="Ventas">Ventas</option>
-
-                      </select>
-
-                    </div>
-
-                  </div>
-
-                  <!-- SECCION DE FOTO DEL USUARIO -->
-
-                  <div class="form-group">
-
-                    <div class="panel">AGREGAR FOTO DE USUARIO</div>
-
-                    <input type="file" name="RegistroFoto" id="RegistroFoto">
-
-                    <p class="help-block"><i class="fas fa-exclamation-circle"></i> Foto no debe superar los 200MB</p>
-
-                    <img src="Views/img/Usuarios/Default/DefaultUser1.png" class="img-thumbnail" width="100px">
-
-
-                  </div>
 
                 </div>
 
               </div>
 
-              <!-- 
+
+              <!-- SECCION NOMBRE DEL USUARIO -->
+
+              <div class="form-group">
+
+                <div class="input-group">
+
+                  <span class="input-group-addon"> <i class="fas fa-user-tag"></i></span>
+
+                  <input type="text" class="form-control input-lg" name="RegitroUsuario" placeholder="Ingresar usuario" required>
+
+                </div>
+
+              </div>
+
+
+              <!-- SECCION CONTRASEÑA DEL USUARIO -->
+
+              <div class="form-group">
+
+                <div class="input-group">
+
+                  <span class="input-group-addon"> <i class="fas fa-key"></i></span>
+
+                  <input type="text" class="form-control input-lg" name="RegitroPassword" placeholder="Ingresar la contraseña" required>
+
+                </div>
+
+              </div>
+
+
+
+              <!-- SECCION PERFIL DEL USUARIO -->
+
+              <div class="form-group">
+
+                <div class="input-group">
+
+                  <span class="input-group-addon"><i class="fas fa-user-shield"></i></span>
+
+                  <select class="form-control input-lg" name="RegistroPerfil">
+
+                    <option value="">Seleccionar el rol</option>
+
+                    <option value="Administrador">Administrador</option>
+
+                    <option value="Inventario">Inventario</option>
+
+                    <option value="Ventas">Ventas</option>
+
+                  </select>
+
+                </div>
+
+              </div>
+
+              <!-- SECCION DE FOTO DEL USUARIO -->
+
+              <div class="form-group">
+
+                <div class="panel">AGREGAR FOTO DE USUARIO</div>
+
+                <input type="file" name="RegistroFoto" id="RegistroFoto">
+
+                <p class="help-block"><i class="fas fa-exclamation-circle"></i> Foto no debe superar los 200MB</p>
+
+                <img src="Views/img/Usuarios/Default/DefaultUser1.png" class="img-thumbnail" width="100px">
+
+
+              </div>
+
+            </div>
+
+          </div>
+
+          <!-- 
             //-------------------------------------------------//
             //       FOOTER VENTANA MODAL                    //
             //-----------------------------------------------//
             -->
 
-              <div class="modal-footer">
+          <div class="modal-footer">
 
-                <button type="button" class="btn btn-primary pull-left" data-dismiss="modal"> <i class="fas fa-times"></i> Cerrar formulario</button>
+            <button type="button" class="btn btn-primary pull-left" data-dismiss="modal"> <i class="fas fa-times"></i> Cerrar formulario</button>
 
-                <button type="submit" class="btn btn-success" data-dismiss="modal"> Guardar usuario <i class="fa fa-user-check"></i></button>
+            <button type="submit" class="btn btn-success" data-dismiss="modal"> Guardar usuario <i class="fa fa-user-check"></i></button>
 
-              </div>
+          </div>
 
         </form>
 
