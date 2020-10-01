3fadeee459f4d4bf1c8db31c1280885e564176b7
luisalbertmschz <cerebrito07@hotmail.com>
2020-10-01 02:04:24 -0400

    [🎉] Cambios Generales Update v2.0

--- Views/Modules/Footer.php
+++
@@ -1,9 +0,0 @@
-  <link rel="stylesheet" href="Views/dist/css/BotonAyuda.css">
-  <footer class="main-footer">
-
-  	<strong>Copyright &copy; 2020 <a href="https://github.com/luisalbertmschz" target="_blank">💖Desarrollado con amor por un grupo dedicado</a>.</strong>
-
-  	Todos los derechos reservados.
-
-
-  </footer>
